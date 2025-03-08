b3440934d27dc365c4fc099fef61a53a1cd3a895
ktyouta <ktyouta0506@gmail.com>
2025-03-09 00:29:12 +0900

    ホーム画面のリファクタリング

--- src/Home/Component/HomeVideoDetail.tsx
+++ src/Home/Component/HomeVideoDetail.tsx
@@ -4,6 +4,8 @@ import LoadingBase from "../../Common/Component/LoadingBase";
 import styled from "styled-components";
 import ButtonComponent from "../../Common/Component/ButtonComponent";
 import { VideoUrlModel } from "../../Common/Model/VideoUrlModel";
+import { HomeVideoDetailInfo } from "./HomeVideoDetailInfo";
+import { HomeVideoDetailMenu } from "./HomeVideoDetailMenu";
 
 
 const Parent = styled.div`
@@ -21,51 +23,6 @@ const VideoContentDiv = styled.div`
   padding-right:5%;
 `;
 
-const VideoInfoDiv = styled.div`
-  width: 60%;
-  height: 100%;
-`;
-
-const VideoImg = styled.img`
-    width: 100%;
-    height: 80%;
-    border-radius: 6%;
-`;
-
-const VideoDescriptionDiv = styled.div`
-  height: 20%;
-`;
-
-const VideoTitle = styled.h3`
-  color:white;
-`;
-
-const ChennelTitleDiv = styled.div`
-  color:white;
-  font-size: 17px;
-`;
-
-const MenuParentDiv = styled.div`
-  width: 34%;
-  height: 675px;
-  margin-left: 3%;
-  margin-top: 5%;
-  box-sizing:border-box;
-`;
-
-const MenuListDiv = styled.div`
-  box-sizing:border-box;
-  height: 70%;
-  background-color: #37475A;
-  border-radius: 2%;
-  border: solid 1px;
-`;
-
-const MenuButtonDiv = styled.div`
-  box-sizing:border-box;
-  padding:3%;
-`;
-
 
 export function HomeVideoDetail() {
 
@@ -81,52 +38,18 @@ export function HomeVideoDetail() {
     return <LoadingBase />;
   }
 
-  const snippet = videoDetail?.snippet;
-  // サムネイルURL
-  const imgUrl = snippet?.thumbnails.high?.url;
-  // タイトル
-  const title = snippet?.title;
-  // チャンネル名
-  const channelTitle = snippet?.channelTitle;
-  // 動画URL
-  const videoUrlModel = new VideoUrlModel(videoId);
-
   return (
     <Parent>
       <VideoContentDiv>
         {/* 動画情報 */}
-        <VideoInfoDiv>
-          <VideoImg
-            src={imgUrl}
-          />
-          <VideoDescriptionDiv>
-            <VideoTitle>
-              {title}
-            </VideoTitle>
-            <ChennelTitleDiv>
-              {channelTitle}
-            </ChennelTitleDiv>
-          </VideoDescriptionDiv>
-        </VideoInfoDiv>
+        <HomeVideoDetailInfo
+          videoDetail={videoDetail}
+        />
         {/* メニュー */}
-        <MenuParentDiv>
-          <MenuListDiv>
-            <MenuButtonDiv>
-              <ButtonComponent
-                styleTypeNumber="GRAD_GRAY"
-                title={"動画を視聴する"}
-                onclick={() => {
-                  window.open(`${videoUrlModel.videoUrl}`, `_blank`);
-                }}
-                style={{
-                  "fontSize": "0.9rem",
-                  "height": "7%",
-                  "width": "100%",
-                }}
-              />
-            </MenuButtonDiv>
-          </MenuListDiv>
-        </MenuParentDiv>
+        <HomeVideoDetailMenu
+          videoDetail={videoDetail}
+          videoId={videoId}
+        />
       </VideoContentDiv>
     </Parent>
   );
---
+++ src/Home/Component/HomeVideoDetailMenu.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { useHomeVideoDetail } from "../Hook/useHomeVideoDetail";
+import LoadingBase from "../../Common/Component/LoadingBase";
+import styled from "styled-components";
+import ButtonComponent from "../../Common/Component/ButtonComponent";
+import { VideoUrlModel } from "../../Common/Model/VideoUrlModel";
+import { HomeVideoDetailInfo } from "./HomeVideoDetailInfo";
+import { YouTubeDataApiVideoDetailItemType } from "../Type/YouTubeDataApiVideoDetailItemType";
+
+
+const MenuParentDiv = styled.div`
+  width: 34%;
+  height: 675px;
+  margin-left: 2%;
+  margin-top: 1%;
+  box-sizing:border-box;
+`;
+
+const MenuListDiv = styled.div`
+  box-sizing:border-box;
+  height: 70%;
+  background-color: #181a1e;
+  border-radius: 2%;
+  border: solid 1px;
+`;
+
+const MenuButtonDiv = styled.div`
+  box-sizing:border-box;
+  padding:3%;
+`;
+
+type propsType = {
+    videoDetail: YouTubeDataApiVideoDetailItemType | undefined,
+    videoId: string
+}
+
+
+export function HomeVideoDetailMenu(props: propsType) {
+
+    console.log("HomeVideoDetailMenu render");
+
+    const videoDetail = props.videoDetail;
+    // 動画URL
+    const videoUrlModel = new VideoUrlModel(props.videoId);
+
+    return (
+        <MenuParentDiv>
+            <MenuListDiv>
+                <MenuButtonDiv>
+                    <ButtonComponent
+                        styleTypeNumber="GRAD_GRAY"
+                        title={"動画を視聴する"}
+                        onclick={() => {
+                            window.open(`${videoUrlModel.videoUrl}`, `_blank`);
+                        }}
+                        style={{
+                            "fontSize": "0.9rem",
+                            "height": "7%",
+                            "width": "100%",
+                        }}
+                    />
+                </MenuButtonDiv>
+            </MenuListDiv>
+        </MenuParentDiv>
+    );
+}
