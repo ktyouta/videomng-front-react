d3164bd55693e50dd999b686904aa4a1f78d0a41
ktyouta <ktyouta0506@gmail.com>
2025-07-25 21:11:00 +0900

    フッターにYouTube Data API利用についての注記を追加

--- src/Footer/Component/Footer.tsx
+++ src/Footer/Component/Footer.tsx
@@ -5,6 +5,18 @@ import styled from "styled-components";
 const Parent = styled.div`
   width: 100%;
   height:120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  box-sizing: border-box;
+  padding-bottom: 1%;
+`;
+
+const TermOfUseMessageAreaDiv = styled.div`
+  color: rgba(255, 255, 255, 0.7);
+  font-size: 13px;
+  box-sizing: border-box;
+  padding-left: 1%;
 `;
 
 
@@ -14,7 +26,10 @@ export function Footer() {
 
   return (
     <Parent>
-
+      <TermOfUseMessageAreaDiv>
+        このアプリは YouTube Data API を利用しており、表示されるコンテンツは YouTube の提供するデータに基づいています。<br />
+        本アプリは YouTube 公式サービスではありません。
+      </TermOfUseMessageAreaDiv>
     </Parent>
   );
 }
