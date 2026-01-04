1305b153cd44a217b83ff492fd5fca42bdae0c9d
ktyouta <ktyouta0506@gmail.com>
2026-01-04 18:19:59 +0900

    認証方式の変更に伴うAPI通信処理の修正

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -14,7 +14,7 @@ function authRequestInterceptor(config: InternalAxiosRequestConfig) {
   config.headers.Accept = 'application/json';
 
   if (accessTokenRef) {
-    config.headers['Authorization'] = accessTokenRef;
+    config.headers['Authorization'] = `Bearer ${accessTokenRef}`;
   }
 
   config.withCredentials = true;
